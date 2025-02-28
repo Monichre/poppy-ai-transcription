8cef713e7db161746ccc776dd1df36eadfbb04d9
monichre <liamhellis@gmail.com>
2025-02-28 16:31:46 -0600

    fixed site url env var

--- src/services/assemblyai/get-assembly-token.ts
+++ src/services/assemblyai/get-assembly-token.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const getAssemblyToken = async () => {
-	const res = await axios.post("http://localhost:3000/api/assembly-ai/token");
+	const res = await axios.post(
+		`${process.env.NEXT_PUBLIC_SITE_URL}/api/assembly-ai/token`
+	);
 	return res.data;
 };
